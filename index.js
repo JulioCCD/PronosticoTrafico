794711f5ff6bd7eea1c3019e52e98e10061a1f6b
JulioCCD <147526263+JulioCCD@users.noreply.github.com>
2024-06-08 18:21:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -18,111 +18,45 @@ const server = http.createServer((req, res) => {
 
   <!DOCTYPE html>
 
-  <html lang="es">
+<html lang="es">
 
-  <head>
+<head>
 
-    <meta charset="UTF-8">
+  <meta charset="UTF-8">
 
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Bienvenidos a Radiaciones Solares</title>
+  <title>Mi Página</title>
 
-    <style>
+  <style>
 
-      body {
+    body {
 
-        font-family: Arial, sans-serif;
+      font-family: Arial, sans-serif;
 
-        margin: 0;
+      text-align: center;
 
-        padding: 0;
+      margin-top: 20%;
 
-        background-color: #f2f2f2;
+    }
 
-        text-align: center;
+    h1 {
 
-      }
+      color: #4CAF50;
 
-      header {
+    }
 
-        background-color: #4CAF50;
+  </style>
 
-        color: white;
+</head>
 
-        padding: 20px;
+<body>
 
-      }
+  <h1>Bienvenidos a mi página</h1>
 
-      .container {
+</body>
 
-        margin-top: 50px;
-
-      }
-
-      h1 {
-
-        font-size: 36px;
-
-        margin-bottom: 20px;
-
-      }
-
-      p {
-
-        font-size: 18px;
-
-      }
-
-      .btn {
-
-        display: inline-block;
-
-        background-color: #4CAF50;
-
-        color: white;
-
-        padding: 10px 20px;
-
-        text-decoration: none;
-
-        border-radius: 5px;
-
-        transition: background-color 0.3s ease;
-
-      }
-
-      .btn:hover {
-
-        background-color: #45a049;
-
-      }
-
-    </style>
-
-  </head>
-
-  <body>
-
-    <header>
-
-      <h1>PRONOSTICOS DEL TIEMPO</h1>
-
-    </header>
-
-    <div class="container">
-
-      <h1>BienvenidosS AL PRONOSTICO</h1>
-
-      <p>¡Descubre todo sobre las predicciones DEL TIEMPO EN EL 10!</p>
-
-      <a href="#" class="btn">Empezar</a>
-
-    </div>
-
-  </body>
-
-  </html>
+</html>
 
  `);
 
