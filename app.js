ba467ad5c970e8fe9ef9af3a16b020eb0d68c1f7
JulioCCD <cesar4551651@gmail.comgitgitfghfghJulioCCD>
2024-06-15 10:52:44 -0500

    nt

---
+++ app.js
@@ -0,0 +1,25 @@
+const express=require("express")
+const app=express()
+const path = require('path');
+
+// Configurar la carpeta 'public' para servir archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set("view engine","ejs")
+
+
+app.get("/reco", (req,res)=>{
+    res.render("reco")
+})
+
+app.get("/about", (req,res)=>{
+    res.render("about")
+})
+
+app.get("/estadistica", (req,res)=>{
+    res.render("estadistica")
+})
+
+app.listen(3000, (req,res)=>{
+    console.log("Corriendo en el puerto 3000")
+}) 
